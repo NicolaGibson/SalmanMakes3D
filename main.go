fa93a5f9bdbb6647ee1036ef6dfdad1e7c3be399
Nicola Gibson <nicola.k.gibson@gmail.com>
2020-07-09 14:52:33 +0100

    Restructuring urls, addition endpoints, printing message to screen.

--- main.go
+++ main.go
@@ -9,18 +9,25 @@ import (
 
 
 func main(){
-	http.HandleFunc("/create-employee", createEmployee)
+	http.HandleFunc("/employee/create", createEmployee)
+	http.HandleFunc("/employee/update", updateEmployeeDetails)
+	http.HandleFunc ("/employee/delete", deleteEmployee)
 	http.HandleFunc("/search-ID", getDetailsByID)
 	http.ListenAndServe(":4000", nil)
 }
 
 func createEmployee(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Endpoint hit: Create new employee record")
+	fmt.Fprintln(w,"Endpoint hit: Create new employee record")
+}
+
+func updateEmployeeDetails(w http.ResponseWriter, r *http.Request ){
+	fmt.Fprintln(w,"Endpoint hit: Update employee details.")
+}
+
+func deleteEmployee(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "Endpoint hit: Delete Employee ")
 }
 
 func getDetailsByID(w http.ResponseWriter, r *http.Request){
- fmt.Println("Endpoint hit: Search Details By ID.")
-
-
-
+ fmt.Fprintln(w, "Endpoint hit: Search Details By ID.")
 }
