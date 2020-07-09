3d413fceca6da08683936de55869bb94467ec41a
Nicola Gibson <nicola.k.gibson@gmail.com>
2020-07-09 14:34:09 +0100

    Server running, two endpoints added and working.

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-package salmanMakes
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+
+
+func main(){
+	http.HandleFunc("/create-employee", createEmployee)
+	http.HandleFunc("/search-ID", getDetailsByID)
+	http.ListenAndServe(":4000", nil)
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint hit: Create new employee record")
+}
+
+func getDetailsByID(w http.ResponseWriter, r *http.Request){
+ fmt.Println("Endpoint hit: Search Details By ID.")
+
+
+
+}
