146f0e4a72abd4affd476b0495337c81d4bf4985
Nicola Gibson <nicola.k.gibson@gmail.com>
2020-07-16 16:42:43 +0100

    working hardcoded get request for employee by ID.

--- main.go
+++ main.go
@@ -6,8 +6,7 @@ import (
 	"log"
 	"net/http"
 	"fmt"
-
-	//"time"
+	_ "encoding/json"
 )
 /*
 
@@ -25,35 +24,47 @@ import (
 //returning data from sqlite database with a get request to an  endpoint where some of the values have a null value
 
 type Employee struct {
-	ID int
-	firstName string
-	lastName string
-	dateOfBirth string
-	addressLineOne string
-	addressLineTwo sql.NullString
-	city string
-	postcode string
-	startDate string
-	nextOfKin string
-	position string
-	endDate sql.NullString
-	recordCreatedDate string
+	ID int `json:"id"`
+	firstName string `json:"first_name"`
+	lastName string `json:"last_name"`
+	dateOfBirth string `json:"date_of_birth"`
+	addressLineOne string `json:"address_line_one"`
+	addressLineTwo sql.NullString `json:"address_line_two"`
+	city string `json:"city"`
+	postcode string `json:"postcode"`
+	startDate string `json:"start_date"`
+	nextOfKin string `json:"next_of_kin"`
+	position string `json:"position"`
+	endDate sql.NullString `json:"end_date"`
+	recordCreatedDate string `json:"record_created_date"`
 }
-
 
 func main() {
 	http.HandleFunc("/employees", employeeHandler)
 	//TO DO HOW TO MAKE /123 DYNAMIC
 	http.HandleFunc("/employees/1", employeeByIDHandler)
 	http.HandleFunc("/employees/search/text", employeeByTextSearchHandler)
-	http.HandleFunc("/employees/search/date", employeeByDateHandler)
 	http.ListenAndServe(":4000", nil)
 }
+
+/*var database *sql.DB
+
+func init() {
+	var err error
+	database, err := sql.Open("sqlite3", "employee-database.db")
+if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = database.Ping(); err != nil {
+		log.Fatal(err)
+	}
+} */
 
 func employeeHandler(w http.ResponseWriter, r *http.Request) {
 	switch method := r.Method; method {
 	case "GET":
-		database, err := sql.Open("sqlite3", "employee-database.db")
+	database, err := sql.Open("sqlite3", "employee-database.db")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -76,6 +87,7 @@ func employeeHandler(w http.ResponseWriter, r *http.Request) {
 			log.Fatal(err)
 		}
 		for _, employee := range employees {
+
 			fmt.Fprintf(w, "%b, %s %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n", employee.ID, employee.firstName, employee.lastName, employee.dateOfBirth, employee.addressLineOne, employee.addressLineTwo, employee.city, employee.postcode, employee.startDate, employee.nextOfKin, employee.position, employee.endDate, employee.recordCreatedDate)
 		}
 			fmt.Fprintln(w, "Endpoint hit: Return all employees records")
@@ -88,27 +100,45 @@ func employeeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func employeeByIDHandler(w http.ResponseWriter, r *http.Request) {
-	//	SELECT ID, firstName, lastName, dateOfBirth, addressLineOne, city, postcode, startDate, nextOfKin, position, recordCreatedDate FROM employees WHERE ID = 1
 	switch method := r.Method; method {
-	case "GET":
-		fmt.Fprintln(w, "Endpoint hit: Return all employees by ID")
-	case "PATCH":
-		fmt.Fprintln(w, "Endpoint hit: Update employees record by ID")
-	case "DELETE":
-		fmt.Fprintln(w, "Endpoint hit: Delete employees record by ID")
-	default:
-		fmt.Fprintln(w, "Endpoint hit: This endpoint only supports GET, PATCH and DELETE requests by ID")
+		case "GET":
+			database, err := sql.Open("sqlite3", "employee-database.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			// TODO: pick up id from url path
+			id := 1
+			row := database.QueryRow("SELECT ID, firstName, lastName, dateOfBirth, addressLineOne, addressLineTwo, city, postcode, startDate, nextOfKin, position, endDate, recordCreatedDate FROM employees WHERE ID =$1", id)
+			employee := new(Employee)
+			err = row.Scan(&employee.ID, &employee.firstName, &employee.lastName, &employee.dateOfBirth, &employee.addressLineOne, &employee.addressLineTwo, &employee.city, &employee.postcode, &employee.startDate, &employee.nextOfKin, &employee.position, &employee.endDate, &employee.recordCreatedDate)
+			if err == sql.ErrNoRows{
+				http.NotFound(w, r)
+				return
+			} else if err != nil{
+				fmt.Fprint(w, err)
+				http.Error(w, http.StatusText(500), 500)
+				return
+			}
+			fmt.Fprintf(w, "%d, %s %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n", employee.ID, employee.firstName, employee.lastName, employee.dateOfBirth, employee.addressLineOne, employee.addressLineTwo, employee.city, employee.postcode, employee.startDate, employee.nextOfKin, employee.position, employee.endDate, employee.recordCreatedDate)
+			fmt.Fprintln(w, "Endpoint hit: Return an employees by ID")
+
+		case "PATCH":
+			fmt.Fprintln(w, "Endpoint hit: Update employees record by ID")
+		case "DELETE":
+			fmt.Fprintln(w, "Endpoint hit: Delete employees record by ID")
+		default:
+			fmt.Fprintln(w, "Endpoint hit: This endpoint only supports GET, PATCH and DELETE requests by ID")
+		}
 	}
-}
 
 func employeeByTextSearchHandler(w http.ResponseWriter, r*http.Request){
-	//SELECT* FROM employees WHERE firstName like '%AAA%' OR lastName like '%AAA%' OR position like '%AAA%'
+	//SELECT* FROM employees WHERE firstName like '%AAA%' OR lastName like '%AAA%' OR position like '%AAA% OR startDate like '%1111%' OR endDate like '%1111%' OR recordCreatedDate like '%1111%'
 	switch method := r.Method; method {
 	case "GET":
-		if r.URL.Query().Get("name") != "" || r.URL.Query().Get ("position") != "" {
-			fmt.Fprintln(w, "Endpoint hit: Search for employees by name or position")
+		if r.URL.Query().Get("name") != "" || r.URL.Query().Get ("position") != "" || r.URL.Query().Get("startDate") != "" || r.URL.Query().Get("endDate") != ""  {
+			fmt.Fprintln(w, "Endpoint hit: Search for employees by name, position or date")
 		} else {
-			fmt.Fprintln(w, "Endpoint hit: Please enter name or position to search for employees")
+			fmt.Fprintln(w, "Endpoint hit: Please enter name, position, start or end date to search for employees")
 		}
 	default:
 	fmt.Fprintln(w, "Endpoint hit: This endpoint only supports GET requests")
@@ -116,19 +146,7 @@ func employeeByTextSearchHandler(w http.ResponseWriter, r*http.Request){
 	}
 }
 
-func employeeByDateHandler(w http.ResponseWriter, r*http.Request) {
-	//SELECT* FROM employees WHERE startDate like '%1111%' OR endDate like '%1111%' OR recordCreatedDate like '%1111%'
-	switch method := r.Method; method {
-	case "GET":
-		if r.URL.Query().Get("startDate") != "" || r.URL.Query().Get("endDate") != "" {
-			fmt.Fprintln(w, "Endpoint hit: Search for employees by end date or start date")
-		} else {
-			fmt.Fprintln(w, "Endpoint hit: Please enter end date or start date to search for employees")
-		}
-	default:
-		fmt.Fprintln(w, "Endpoint hit: This endpoint only supports GET requests")
-	}
-}
+
 
 
 
@@ -160,6 +178,8 @@ func employeeByDateHandler(w http.ResponseWriter, r*http.Request) {
 		fmt.Println(employee.ID,":" + " " + employee.firstName, employee.lastName, employee.dateOfBirth, employee.addressLineOne, employee.addressLineTwo, employee.city, employee.postcode, employee.startDate, employee.nextOfKin, employee.position, employee.endDate, employee.recordCreatedDate)
 	}
 } */
+
+
 
 
 /* TO DO
