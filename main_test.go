a5e18d598381eea434d82e63b3058b3008bc18e5
Nicola Gibson <nicola.k.gibson@gmail.com>
2020-09-21 14:56:25 +0100

    outline for tests.

---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestemployeeHandler(t *testing.T){
+// It should return all employees
+//It should create a new employee
+}
+
+func TestgetEmployeeByIDHandler(t *testing.T){
+	// It should return one specific employee when given an ID
+
+}
+
+func TestdeleteEmployeeByIDHandler(t *testing.T){
+	// It should delete one specific employee when given an ID
+}
+
+func TestupdateEmployeeByIDHandler(t *testing.T){
+	// It should update one specific employee's details when given an ID
+
+}
+
+func TestemployeeSearch(t *testing.T){
+	// It should search for all employees that meet search criteria.
+
+}
